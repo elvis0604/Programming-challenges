301eef41e33a78a6bbf733a0d336f40ccb4ab194
elvis0604 <46872849+elvis0604@users.noreply.github.com>
2019-11-22 01:39:04 -0600

    fix a bug in dynamic programming

--- Answers/Dp/Dynamic Programming/DP.java
+++ Answers/Dp/Dynamic Programming/DP.java
@@ -14,7 +14,7 @@ class DP
 
         if (total < 0)
         {
-            return 0;
+            return Integer.MAX_VALUE;
         }
 
         if(map.containsKey(total))
@@ -26,7 +26,7 @@ class DP
         for(int i = n - 1; i >= 0; i--)
         {
             int temp = count(coins, n, total - coins[i], map);
-            if(temp + 1 < min_coin)
+            if(temp != Integer.MAX_VALUE && temp + 1 < min_coin)
                 min_coin = temp + 1;
         }
         map.put(total, min_coin);
@@ -35,7 +35,7 @@ class DP
     
 	public static void main(String[] args) 
 	{ 
-        int arr[] = {1, 2};
+        int arr[] = {1, 2, 4};
         Map<Integer, Integer> map = new HashMap<>();
 		System.out.println(count(arr, arr.length, 4, map)); 
 	} 
--- Answers/Dp/Naive/DP.java
+++ Answers/Dp/Naive/DP.java
@@ -12,7 +12,7 @@ class DP
 
         if (total < 0)
         {
-            return 0;
+            return Integer.MAX_VALUE;
         }
 
         int min_coin = Integer.MAX_VALUE;
@@ -22,7 +22,7 @@ class DP
         for(int i = n - 1; i >= 0; i--)
         {
             int temp = count(coins, n, total - coins[i]);
-            if(temp + 1 < min_coin)
+            if(temp != Integer.MAX_VALUE && temp + 1 < min_coin)
                 min_coin = temp + 1;
         }
         return min_coin;
@@ -30,8 +30,8 @@ class DP
     
 	public static void main(String[] args) 
 	{ 
-        int arr[] = {1, 2};
-		System.out.println(count(arr, arr.length, 4)); 
+        int arr[] = {1, 2, 4, 5};
+		System.out.println(count(arr, arr.length, 8)); 
 	} 
 
 } 
